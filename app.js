5926f5c45920e2553acf2cb3c253c9bab5dbcbaf
Ayush P Gupta <ayushpguptaapg@gmail.com>
2019-03-22 00:01:12 +0530

    Rename stylist api endpoint

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/v1/stylist', stylistRouter);
+app.use('/api/v1/dot/stylist', stylistRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
