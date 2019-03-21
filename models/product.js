410a93419c517ea11ffde7d53ca9682411d11b25
Ayush P Gupta <ayushpguptaapg@gmail.com>
2019-03-22 00:17:08 +0530

    Fix a typo error

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Feed = mongoose.model('feeds', Schema(),);
+const Product = mongoose.model('products', Schema(),);
 
-module.exports = Feed;
+module.exports = Product;
